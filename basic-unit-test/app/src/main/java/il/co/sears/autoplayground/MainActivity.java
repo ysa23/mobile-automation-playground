b05baf72883d9efb20db6380ccd346965cc4c5b0
yossi <yossi@sears.co.il>
2017-11-06 14:40:22 +0200

    Add more layout for UI testing

--- basic-unit-test/app/src/main/java/il/co/sears/autoplayground/MainActivity.java
+++ basic-unit-test/app/src/main/java/il/co/sears/autoplayground/MainActivity.java
@@ -2,12 +2,24 @@ package il.co.sears.autoplayground;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = findViewById(R.id.click_me_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                final TextView textView = findViewById(R.id.hello_text);
+                textView.setText(String.format("Hello. I've been clicked %d times", ++counter));
+            }
+        });
     }
 }
